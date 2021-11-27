0705af3cf0338d8ef4c26eff76cef58831115b61
Huyhsu <siu891014@gmail.com>
2021-11-27 13:04:46 +0800

    Set up enemy behavior using finite state machines

---
+++ Assets/Scripts/Enemies/State Machine/Entity.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Entity : MonoBehaviour
+{
+    public FiniteStateMachine stateMachine;
+
+    public D_Entity entityData;
+    
+    public int facingDirection { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    public Animator anim { get; private set; }
+    public GameObject aliveGameObject { get; private set; }
+
+    private Vector2 velocityWorkspace;
+
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private Transform ledgeCheck;
+    
+    public virtual void Start()
+    {
+        facingDirection = 1;
+        
+        aliveGameObject = transform.Find("Alive").gameObject;
+        rb = aliveGameObject.GetComponent<Rigidbody2D>();
+        anim = aliveGameObject.GetComponent<Animator>();
+
+        stateMachine = new FiniteStateMachine();
+    }
+
+    public virtual void Update()
+    {
+       stateMachine.currentState.LogicUpdate(); 
+    }
+
+    public virtual void FixedUpdate()
+    {
+        stateMachine.currentState.PhysicsUpdate();
+    }
+
+    public virtual void SetVelocity(float velocity)
+    {
+        velocityWorkspace.Set(facingDirection * velocity, rb.velocity.y);
+        rb.velocity = velocityWorkspace;
+    }
+
+    public virtual bool CheckWall()
+    {
+        return Physics2D.Raycast(wallCheck.position, aliveGameObject.transform.right, entityData.wallCheckDistance,entityData.whatIsGround);
+    }
+
+    public virtual bool CheckLedge()
+    {
+        return Physics2D.Raycast(ledgeCheck.position, Vector2.down, entityData.ledgeCheckDistance,entityData.whatIsGround);
+    }
+
+    public virtual void Flip()
+    {
+        facingDirection *= -1;
+        aliveGameObject.transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(wallCheck.position,wallCheck.position + (Vector3)(Vector2.right * facingDirection * entityData.wallCheckDistance));
+        Gizmos.DrawLine(ledgeCheck.position,ledgeCheck.position + (Vector3)(Vector2.down * entityData.wallCheckDistance));
+
+    }
+}
