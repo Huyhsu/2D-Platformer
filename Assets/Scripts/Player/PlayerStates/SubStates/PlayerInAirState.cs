4b960f9692150cb09f3969ba41662b93d52445c4
Huyhsu <siu891014@gmail.com>
2021-12-08 23:43:24 +0800

    Add player wall slide/grab/climb

--- Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
+++ Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
@@ -6,10 +6,12 @@ public class PlayerInAirState : PlayerState
 {
     private int xInput;
     private bool isGrounded;
+    private bool isTouchingWall;
     private bool jumpInput;
     private bool jumpInputStop;
     private bool coyoteTime;
     private bool isJumping;
+    private bool grabInput;
     public PlayerInAirState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
     }
@@ -19,6 +21,7 @@ public class PlayerInAirState : PlayerState
         base.DoCheck();
 
         isGrounded = player.CheckIfGrounded();
+        isTouchingWall = player.CheckIfTouchingWall();
     }
 
     public override void Enter()
@@ -40,6 +43,7 @@ public class PlayerInAirState : PlayerState
         xInput = player.InputHandler.NormalizedInputX;
         jumpInput = player.InputHandler.JumpInput;
         jumpInputStop = player.InputHandler.JumpInputStop;
+        grabInput = player.InputHandler.GrabInput;
         
         CheckJumpMultiplier();
         
@@ -49,7 +53,16 @@ public class PlayerInAirState : PlayerState
         }
         else if (jumpInput && player.JumpState.CanJump())
         {
+            player.InputHandler.UseJumpInput();
             stateMachine.ChangeState(player.JumpState);
+        }
+        else if (isTouchingWall && grabInput)
+        {
+            stateMachine.ChangeState(player.WallGrabState);
+        }
+        else if (isTouchingWall && xInput == player.FacingDirection && player.CurrentVelocity.y <= 0f)
+        {
+            stateMachine.ChangeState(player.WallSlideState);
         }
         else
         {
--- Assets/Scripts/Player/PlayerStates/SuperStates/PlayerGroundedState.cs
+++ Assets/Scripts/Player/PlayerStates/SuperStates/PlayerGroundedState.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerGroundedState : PlayerState
 {
     protected int xInput;
+    
     private bool jumpInput;
+    private bool grabInput;
     private bool isGrounded;
+    private bool isTouchingWall;
     
     public PlayerGroundedState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
@@ -18,6 +21,7 @@ public class PlayerGroundedState : PlayerState
         base.DoCheck();
 
         isGrounded = player.CheckIfGrounded();
+        isTouchingWall = player.CheckIfTouchingWall();
     }
 
     public override void Enter()
@@ -38,6 +42,7 @@ public class PlayerGroundedState : PlayerState
 
         xInput = player.InputHandler.NormalizedInputX;
         jumpInput = player.InputHandler.JumpInput;
+        grabInput = player.InputHandler.GrabInput;
 
         if (jumpInput && player.JumpState.CanJump())
         {
@@ -48,6 +53,10 @@ public class PlayerGroundedState : PlayerState
         {
             player.InAirState.StartCoyoteTime();
             stateMachine.ChangeState(player.InAirState);
+        }
+        else if (isTouchingWall && grabInput)
+        {
+            stateMachine.ChangeState(player.WallGrabState);
         }
     }
 
